0487d651422435c9629e2557255f605b977bc6a5
jaibles <ciaran@visible.vc>
2021-01-04 19:26:10 +0000

    test

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,6 @@ module.exports = function (eleventyConfig) {
  
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
 
-  eleventyConfig.addPassthroughCopy('js/highcharts.js');
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/fonts');
 
